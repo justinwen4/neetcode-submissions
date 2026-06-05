20cb65085bf97262f77344ebf0f28a96327423a3
Justin Wen <justinwen4@gmail.com>
2026-06-04 21:25:04 -0400

    Add: baseball-game - submission-9

---
+++ Data Structures & Algorithms/baseball-game/submission-9.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for op in operations:
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif op == 'D':
+                stack.append(stack[-1] * 2)
+            elif op == 'C':
+                stack.pop()
+            else:
+                stack.append(int(op))
+        
+        points = 0
+
+        for each in stack:
+            points += each
+
+        return points
